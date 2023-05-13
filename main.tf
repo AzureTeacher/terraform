044a6cc0c8713259ac317c863c4ff3877cb076a1
Tom Wechsler <tom@tomwechsler.ch>
2023-05-13 14:47:02 +0200

    Terraform

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_resource_group" "twrg" {
+  name     = var.azure_rg_name
+  location = var.azure_location
+
+  tags = local.tags
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "azure_rg_name" {
+  type        = string
+  description = "The Azure RG Name"
+  default     = "tw-prod2023-rg"
+}
+
+variable "azure_location" {
+  type        = string
+  description = "The Azure Location"
+  default   = "West Europe"
+}
